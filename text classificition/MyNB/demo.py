3fd3b6ad2f46648f20db7e385479308cbb76b0f6
李亚国 <1075543143@qq.com>
2018-11-02 14:06:00 +0800

    贝叶斯分类器

--- text classificition/MyNB/demo.py
+++ text classificition/MyNB/demo.py
@@ -52,6 +52,11 @@ def createVocabList(dataSet):
     return list(vocabSet)
 
 
+# if __name__ == '__main__':
+#     postingList, classVec = loadDataSet()
+#     print(createVocabList(postingList))
+
+
 def setOfWords2Vec(vocabList, inputSet):
     '''  词1,词2,XXX，词n    #词表vocabList
 
@@ -75,21 +80,34 @@ def setOfWords2Vec(vocabList, inputSet):
 
     return returnVec
 
+# if __name__ == '__main__':
+#     postingList, classVec = loadDataSet()
+#     vocabList=createVocabList(postingList)
+#     inputSet = ['star','yello','dalmation','worthless','love']
+#     print(setOfWords2Vec(vocabList,inputSet))
+
+
 
 def trainNB0(trainMatrix, trainCategory):
-    numTrainDocs =len(trainMatrix)  # 文档的个数
+    """
 
-    numWords = len(trainMatrix[0])  # 词表的个数
+    :param trainMatrix: [[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0], [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1], [1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0]]
+    :param trainCategory:[0,1,0,1,0,1]
+    :return:
+    """
+    numTrainDocs =len(trainMatrix)  # 文档的个数6
 
-    pAbusive = sum(trainCategory) / float(numTrainDocs)  # sum([0,1,0,1,0,1])=3,也即是trainCategory里面1的个数
+    numWords = len(trainMatrix[0])  # 词表的个数32
 
-    p0Num = np.zeros(numWords)  # type(p0Num)为numpy.array类型
+    pAbusive = sum(trainCategory) / float(numTrainDocs)  # sum([0,1,0,1,0,1])=3,也即是trainCategory里面1的个数 #先验概率
 
-    p1Num = np.zeros(numWords)
+    p0Num = np.zeros(numWords)  # type(p0Num)为numpy.array类型  负例词在词表中的分布
 
-    p0Denom = 0.0
+    p1Num = np.zeros(numWords)   #正例词在词表中的分布
 
-    p1Denom = 0.0
+    p0Denom = 0.0     #负例词的个数
+
+    p1Denom = 0.0      #正例词的个数
 
     for i in range(numTrainDocs):  # 对于每个文档进行处理
 
@@ -107,11 +125,14 @@ def trainNB0(trainMatrix, trainCategory):
 
     '''将正反例中的实例词向量分别叠加，分别除以正反例中词的个数，计算p(w0|ci),...,p(wn|ci)'''
 
-    p1Vect = p1Num / p1Denom  # 所有正例实例按行叠加的向量p1Num，除以所有正例文档中词的个数p1Denom
+    p1Vect = p1Num / p1Denom  # 所有正例实例按行叠加的向量p1Num，除以所有正例文档中词的个数p1Denom  正例词的概率分布
 
-    p0Vect = p0Num / p0Denom  # p0Num为numpy.array类型，故可以array类型除以数字
+    p0Vect = p0Num / p0Denom  # p0Num为numpy.array类型，故可以array类型除以数字   负例词的概率分布
 
     return p0Vect, p1Vect, pAbusive
+
+
+
 
 
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
@@ -122,20 +143,27 @@ def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
     log(p(w|ci)*p(ci))=log(p(w0|ci)*p(w1|ci)*...p(wn|ci)*p(ci) = sum(log(p(wi|ci))) + log(p(ci))
 
     '''
-
-    p1 = sum(vec2Classify * p1Vec) + np.log(pClass1)
-    print('p1:', p1)
-
-    p0 = sum(vec2Classify * p0Vec) + np.log(1.0 - pClass1)
-    print('p0:', p0)
-
-    if p1 > p0:
-
-        return 1
-
-    else:
-
+    p1_vec=vec2Classify * p1Vec
+    p0_vec=vec2Classify * p0Vec
+    if np.sum(p1_vec)==0.0 and np.sum(p0_vec) !=0.0:
         return 0
+    elif np.sum(p0_vec) == 0.0 and np.sum(p1_vec)!=0.0:
+        return 1
+    elif np.sum(p1_vec) ==0.0 and np.sum(p0_vec) ==0.0:
+        return None
+    else:
+        p1 = sum(p1_vec) + np.log(pClass1)
+        print('p1:', p1)
+        p0 = sum(p0_vec) + np.log(1.0 - pClass1)
+        print('p0:', p0)
+
+        if p1 > p0:
+
+            return 1
+
+        else:
+
+            return 0
 
 
 def testingNB():
@@ -155,21 +183,36 @@ def testingNB():
         trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
 
     p0V, p1V, pAb = trainNB0(trainMat, listClasses)
-    print(p0V)
-    print(p1V)
+    # print(p0V)  #[ 0.04166667  0.08333333  0.04166667  0.04166667  0.04166667  0.04166667 0.04166667  0.04166667  0.04166667  0.04166667  0. 0. ...]
+
+    # print(p1V)  #[ 0.          0.05263158  0.          0.10526316  0.05263158  0.          0. ...]
 
     testEntry = ['love', 'my', 'dalmation', 'like']
     #     print(myVocabList)
 
-    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
-    print(thisDoc)
-    print(testEntry, "classified as: ", classifyNB(thisDoc, p0V, p1V, pAb))
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))  #[0 0 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0]
+    print(testEntry, "classified as: ", classifyNB(thisDoc, p0V, p1V, pAb))  #['love', 'my', 'dalmation', 'like'] classified as:  0
 
-    testEntry = ["stupid", "garbage"]
+    testEntry = ["li", "zii",'heh','buying', 'problems','buying','buying','worthless']
 
-    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
-
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))  #['stupid', 'garbage'] classified as:  1
+    #
     print(testEntry, "classified as: ", classifyNB(thisDoc, p0V, p1V, pAb))
 
 if __name__ == '__main__':
     testingNB()
+    """
+    贝叶斯分类器：
+    1读取所有文档，进行分词处理，去掉虚词，构建词表
+    2构建每个类别的先验概率，和每一个类别中词汇在总词表中概率分布情况
+        2.1去重处理
+        2.2 不去重处理
+    3 计算新文档分类：
+        3.1 构建新文档在总词表中的频数分布
+            3.1.1 去重
+            3.1.2 不去重
+        3.2 将先文档词表频数分布与之前每一个类别词表概率分布相乘
+            log(p(ci|w))∝
+             log(p(w|ci)*p(ci))=log(p(w0|ci)*p(w1|ci)*...p(wn|ci)*p(ci) 
+             = sum(log(p(wi|ci))) + log(p(ci))
+    """
